92eb714866fbbaf9d18f33cb88888cc8712e1842
sanre <sanre@X>
2023-11-10 16:51:19 -0500

    problemaArchivos

--- src/ProblemaArchivos/Main.java
+++ src/ProblemaArchivos/Main.java
@@ -13,20 +13,20 @@ public class Main {
         String rutaArchivo = "Hola.txt";
 
         try {
-            // Intenta abrir el archivo
+           
             FileReader archivo = new FileReader(rutaArchivo);
             BufferedReader lector = new BufferedReader(archivo);
 
-            // Lee línea por línea e imprime en la consola
+           
             String linea;
             while ((linea = lector.readLine()) != null) {
                 System.out.println(linea);
             }
 
-            // Cierra el lector
+            
             lector.close();
         } catch (FileNotFoundException e) {
-            // Maneja la excepción si el archivo no se encuentra
+           
             System.out.println("Error: Archivo no encontrado");
         }
     }
